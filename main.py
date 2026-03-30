1ae37d556022a103fa07e9d9abdbece51b0960d6
Pavlandr1 <pavlandr1@gmail.com>
2026-03-30 15:31:30 +0300

    asteroid_seal initial implementation

---
+++ main.py
@@ -0,0 +1,30 @@
+import pygame
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from logger import log_state
+from player import Player
+
+def main():
+    print(f"Starting Asteroids with pygame version:{pygame.version.ver}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        player.draw(screen)
+        pygame.display.flip()
+        time_passed_ms = clock.tick(60)
+        dt = time_passed_ms/1000
+
+
+if __name__ == "__main__":
+    main()
